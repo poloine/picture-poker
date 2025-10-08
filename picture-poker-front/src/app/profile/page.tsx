d01546bfd3b9095baaf4f142435b68e91136a11f
Antoine B <aberthillot@myges.fr>
2025-10-08 11:52:23 +0200

    Created profile page

---
+++ picture-poker-front/src/app/profile/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import {useAuth} from "@/context/AuthContext";
+
+export default function ProfilePage() {
+    const { user, loading, logout } = useAuth();
+
+    if (loading) return <p>Chargement...</p>;
+    if (!user) return <p>Non connecté</p>;
+
+    if (!user) return <p className="text-center mt-20">Chargement...</p>;
+
+    return (
+        <div className="max-w-lg mx-auto bg-base-100 shadow-lg rounded-lg p-6 mt-10">
+            <h1 className="text-2xl font-bold mb-4 text-center">Mon profil</h1>
+            <p><strong>Nom d’utilisateur :</strong> {user.username}</p>
+            <p><strong>Email :</strong> {user.email}</p>
+            <p><strong>Score :</strong> {user.score}</p>
+            <p><strong>Pièces :</strong> {user.money}</p>
+            <button onClick={logout} className="btn btn-error mt-6 w-full">
+                Se déconnecter
+            </button>
+        </div>
+    );
+}
