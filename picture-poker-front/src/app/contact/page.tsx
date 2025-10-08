b834a5728dc4b528cf5df6896877ad62b323c292
Antoine B <aberthillot@myges.fr>
2025-10-08 11:52:59 +0200

    Created contact page WIP

---
+++ picture-poker-front/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() { //TODO: Complete
+    return (
+        <div className="max-w-2xl mx-auto text-center py-10">
+            <h1 className="text-2xl font-semibold mb-4">Contact & Informations</h1>
+            <p>Ce projet est un jeu en ligne inspiré de Picture Poker de Mario 64 DS.</p>
+            <p className="mt-2">Pour tout contact : <a href="mailto:contact@picturepoker.dev" className="text-primary">contact@picturepoker.dev</a></p>
+        </div>
+    );
+}
