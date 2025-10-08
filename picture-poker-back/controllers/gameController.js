4b3798262148155661a86a58121736dd0101851e
Antoine B <aberthillot@myges.fr>
2025-10-08 11:53:35 +0200

    changed gameController to module

--- picture-poker-back/controllers/gameController.js
+++ picture-poker-back/controllers/gameController.js
@@ -1,28 +1,32 @@
 import prisma from "../prisma/client.js";
 
-export const getGameDetail = async (req, res) => {
-    const { uuid } = req.params;
+const gameController = {
+    getGameDetail: async (req, res) => {
+        const { uuid } = req.params;
 
-    try {
-        const game = await prisma.game.findUnique({
-            where: { uuid },
-            include: {
-                players: {
-                    include: { user: true }
-                },
-                round: true
-            }
-        });
+        try {
+            const game = await prisma.game.findUnique({
+                where: { uuid },
+                include: {
+                    players: {
+                        include: { user: true }
+                    },
+                    round: true
+                }
+            });
 
-        if (!game) return res.status(404).json({ error: "Game not found" });
+            if (!game) return res.status(404).json({ error: "Game not found" });
 
-        // Vérifier si l'utilisateur a participé à la partie
-        const isPlayer = game.players.some(p => p.user_id === req.user.id);
-        if (!isPlayer) return res.status(403).json({ error: "Not allowed" });
+            // Vérifier si l'utilisateur a participé à la partie
+            const isPlayer = game.players.some(p => p.user_id === req.user.id);
+            if (!isPlayer) return res.status(403).json({ error: "Not allowed" });
 
-        res.json(game);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
+            res.json(game);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Internal server error" });
+        }
     }
-};
+}
+
+export default gameController;
